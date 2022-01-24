5a9d586fbe0e6b85b233533a1fd4f69e9a9bd174
Aseem Sethi <aseemsethi@yahoo.com>
2022-01-24 11:23:46 +0000

    added DB mysql template file

--- db/db.go
+++ db/db.go
@@ -1,11 +1,69 @@
 package db
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"os"
 )
 
-// We write a JSON file that is read by DB_init()
+type Customer struct {
+	Cid      int    `json:"cid"`
+	Name     string `json:"name"`
+	Location string `json:"location"`
+	Address  string `json:"address"`
+}
+
+/*
+mysql> CREATE USER 'root'@'%' IDENTIFIED BY 'PASSWORD';
+mysql> GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+mysql> FLUSH PRIVILEGES;
+*/
+
 func Db_init() {
-	fmt.Printf("\n Db_init called")
-	// read in gurupada_db.json file and build out the DB
+	fmt.Println("Connect to MySQL IOT DB")
+	fmt.Println("Reading Environment Variable")
+	var databasePass string
+	databasePass = os.Getenv("DATABASE_PASS")
+	fmt.Printf("Database Password: %s\n", databasePass)
+	connString := "root:" + databasePass + "@tcp(15.206.73.249:3306)/IOT"
+	//fmt.Println("Conn string: ", connString)
+
+	// Open up our database connection.
+	db, err := sql.Open("mysql", connString)
+	// defer the close till after the main function has finished executing
+	defer db.Close()
+
+	// if there is an error opening the connection, handle it
+	if err != nil {
+		panic(err.Error())
+	}
+	// Open doesn't open a connection. Validate DSN data:
+	err = db.Ping()
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+
+	fmt.Println("Connection succeeded...")
+
+	// Execute the query
+	insert, err := db.Query("INSERT INTO customer VALUES ( 99, 'PMOA', 'Bangalore', 'Varthur')")
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+	defer insert.Close()
+
+	results, err := db.Query("SELECT * FROM customer")
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+	for results.Next() {
+		var customer Customer
+		err = results.Scan(&customer.Cid, &customer.Name, &customer.Location, &customer.Address)
+		if err != nil {
+			panic(err.Error()) // proper error handling instead of panic in your app
+		}
+		// and then print out the tag's Name attribute
+		fmt.Printf("%v", customer)
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/aseemsethi/iotus
 
 go 1.15
 
-require github.com/eclipse/paho.mqtt.golang v1.3.5
+require (
+	github.com/aws/aws-sdk-go-v2 v1.13.0
+	github.com/aws/aws-sdk-go-v2/service/rds v1.16.0
+	github.com/eclipse/paho.mqtt.golang v1.3.5
+	github.com/go-sql-driver/mysql v1.6.0
+)
